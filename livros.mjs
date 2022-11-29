552c7204b803d5e82091a76b97c38d47d15d7011
Jotagê <71993919+JG-entusiasta@users.noreply.github.com>
2022-11-29 15:04:48 -0300

    Add files via upload

--- livros.mjs
+++ livros.mjs
@@ -2,11 +2,32 @@ import fs from "fs";
 import path from "path";
 import fetch from "node-fetch";
 
+const email = "20100092@educargloria.com";
+const password = "educar1234#";
+
 var startTime = Date.now();
 
 var il = 0;
 
+var auth = "";
+
+async function login() {
+	var f = await fetch("https://auth-server.portalsas.com.br/v2/auth", {
+		"headers": {
+			"accept": "application/json, text/plain, */*",
+			"content-type": "application/json",
+		},
+		"body": "{\"login\":\"" + email + "\",\"password\":\"" + password + "\"}",
+		"method": "POST"
+	});
+	var json = await f.json();
+
+	return "Bearer " + json.authResponse.token; 
+}
+
 async function main() {
+	auth = await login();
+
 	var livros = await getLivros();
 
 	for (var i = 0; i < livros.length; i++) {
@@ -29,7 +50,7 @@ async function getLivros() {
 				var f = await fetch("https://digital-content-bff.sasdigital.com.br/v1/materials?pageNumber=" + k, {
 					"headers": {
 						"accept": "application/json, text/plain, */*",
-						"authorization": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxODc5ODI2IiwidXNlciI6eyJkZXRhaWxzIjpudWxsLCJpZCI6MTg3OTgyNiwibmFtZSI6Ikpvw6NvIEd1aWxoZXJtZSBTYW50b3MgU2lsdmEiLCJzY2hvb2wiOnsiaWQiOjk3ODUsIm5hbWUiOiJDT0xFR0lPIEVEVUNBUiAtIFNFIChOb3NzYSBTZW5ob3JhIGRhIEdsw7NyaWEpIn0sImVkdWNhdGlvbmFsU3lzdGVtIjp7ImlkIjoxLCJuYW1lIjoiU0FTIn0sImxvY2F0aW9uSWQiOjI4MDQ1MDgsInByb2ZpbGVzIjpbeyJpZCI6MiwibmFtZSI6IkFsdW5vIn1dLCJncmFkZXMiOlt7ImlkIjo0LCJuYW1lIjoiUHLDqS1Vbml2ZXJzaXTDoXJpbyIsImNvbGxlY3Rpb25JZCI6OSwib3JkZXIiOjE2LCJjb250cmFjdFR5cGUiOiJERUZBVUxUIiwiZXVyZWthQ29udGVudFllYXIiOjIwMjJ9XSwiY29udGVudFllYXIiOjIwMjIsImRlZmF1bHRDb2xsZWN0aW9uSWQiOjksImFjY2VwdGVkVGVybXNPZlNlcnZpY2VWZXJzaW9uIjoxLCJhY2NlcHRlZERhdGFTaGFyZVBvbGljeSI6dHJ1ZSwibG9nZ2VkVXNlckFkbWluIjpmYWxzZSwiZ3N1aXRlRW1haWwiOiIyMDEwMDA5Mi54emZ4enFAYXVsYS5wb3J0YWxzYXMuY29tLmJyIn0sImlhdCI6MTY2OTYzNjgzNSwiZXhwIjoxNjY5NzIzMjM1fQ.YS6--DKvUIB8o-D87xZg9cnGF9GFYI1IEZcHTy6lMEvwz-tGWEgTw7FDNwAo4J5UhB9hIntpKLEYlSwtW2SlGQ"
+						"authorization": auth 
 					},
 					"method": "GET"
 				});
@@ -59,7 +80,7 @@ async function Topics(id, livro) {
 			var f = await fetch("https://digital-content-bff.sasdigital.com.br/v1/materials/" + id + "?includeNotVisibleContents=true", {
 				"headers": {
 					"accept": "application/json, text/plain, */*",
-					"authorization": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxODc5ODI2IiwidXNlciI6eyJkZXRhaWxzIjpudWxsLCJpZCI6MTg3OTgyNiwibmFtZSI6Ikpvw6NvIEd1aWxoZXJtZSBTYW50b3MgU2lsdmEiLCJzY2hvb2wiOnsiaWQiOjk3ODUsIm5hbWUiOiJDT0xFR0lPIEVEVUNBUiAtIFNFIChOb3NzYSBTZW5ob3JhIGRhIEdsw7NyaWEpIn0sImVkdWNhdGlvbmFsU3lzdGVtIjp7ImlkIjoxLCJuYW1lIjoiU0FTIn0sImxvY2F0aW9uSWQiOjI4MDQ1MDgsInByb2ZpbGVzIjpbeyJpZCI6MiwibmFtZSI6IkFsdW5vIn1dLCJncmFkZXMiOlt7ImlkIjo0LCJuYW1lIjoiUHLDqS1Vbml2ZXJzaXTDoXJpbyIsImNvbGxlY3Rpb25JZCI6OSwib3JkZXIiOjE2LCJjb250cmFjdFR5cGUiOiJERUZBVUxUIiwiZXVyZWthQ29udGVudFllYXIiOjIwMjJ9XSwiY29udGVudFllYXIiOjIwMjIsImRlZmF1bHRDb2xsZWN0aW9uSWQiOjksImFjY2VwdGVkVGVybXNPZlNlcnZpY2VWZXJzaW9uIjoxLCJhY2NlcHRlZERhdGFTaGFyZVBvbGljeSI6dHJ1ZSwibG9nZ2VkVXNlckFkbWluIjpmYWxzZSwiZ3N1aXRlRW1haWwiOiIyMDEwMDA5Mi54emZ4enFAYXVsYS5wb3J0YWxzYXMuY29tLmJyIn0sImlhdCI6MTY2OTYzNjgzNSwiZXhwIjoxNjY5NzIzMjM1fQ.YS6--DKvUIB8o-D87xZg9cnGF9GFYI1IEZcHTy6lMEvwz-tGWEgTw7FDNwAo4J5UhB9hIntpKLEYlSwtW2SlGQ"
+					"authorization": auth
 				},
 				"method": "GET"
 			});
@@ -89,7 +110,7 @@ async function Topics(id, livro) {
 async function InitPDF(id, livro, name) {
 	var pages = [];
 
-	var i = 2;
+	var i = 0;
 
 	while (true) {
 		// console.log("3 iteration");
@@ -118,7 +139,7 @@ async function getImage(id, n) {
 			var f = await fetch("https://pageflip.portalsas.com.br/" + id + "/files/large/" + n + ".jpg", {
 				"headers": {
 					"accept": "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
-					"cookie": "_gid=GA1.3.1470628385.1669639167; ln_or=d; _hjFirstSeen=1; _hjSession_1632105=eyJpZCI6IjExMTNjNTEzLWQ3MDktNDE3NS04MzRlLWQyMTBhN2EyMTkzOSIsImNyZWF0ZWQiOjE2Njk2MzkxNjgwOTksImluU2FtcGxlIjpmYWxzZX0=; token=Bearer%20eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxODc5ODI2IiwidXNlciI6eyJkZXRhaWxzIjpudWxsLCJpZCI6MTg3OTgyNiwibmFtZSI6Ikpvw6NvIEd1aWxoZXJtZSBTYW50b3MgU2lsdmEiLCJzY2hvb2wiOnsiaWQiOjk3ODUsIm5hbWUiOiJDT0xFR0lPIEVEVUNBUiAtIFNFIChOb3NzYSBTZW5ob3JhIGRhIEdsw7NyaWEpIn0sImVkdWNhdGlvbmFsU3lzdGVtIjp7ImlkIjoxLCJuYW1lIjoiU0FTIn0sImxvY2F0aW9uSWQiOjI4MDQ1MDgsInByb2ZpbGVzIjpbeyJpZCI6MiwibmFtZSI6IkFsdW5vIn1dLCJncmFkZXMiOlt7ImlkIjo0LCJuYW1lIjoiUHLDqS1Vbml2ZXJzaXTDoXJpbyIsImNvbGxlY3Rpb25JZCI6OSwib3JkZXIiOjE2LCJjb250cmFjdFR5cGUiOiJERUZBVUxUIiwiZXVyZWthQ29udGVudFllYXIiOjIwMjJ9XSwiY29udGVudFllYXIiOjIwMjIsImRlZmF1bHRDb2xsZWN0aW9uSWQiOjksImFjY2VwdGVkVGVybXNPZlNlcnZpY2VWZXJzaW9uIjoxLCJhY2NlcHRlZERhdGFTaGFyZVBvbGljeSI6dHJ1ZSwibG9nZ2VkVXNlckFkbWluIjpmYWxzZSwiZ3N1aXRlRW1haWwiOiIyMDEwMDA5Mi54emZ4enFAYXVsYS5wb3J0YWxzYXMuY29tLmJyIn0sImlhdCI6MTY2OTYzOTE3MywiZXhwIjoxNjY5NzI1NTczfQ.0yO_scStYazMdFtmdK2f1ynzH7Fp0pVm9dc8Byzwvjnj_IgK4-kmnnixm9oVe9R3WolJyFybxqE70Ih8dLJelw; strategy=token; session=GFL8fGeCM67q7JfJZL95aBIe8RNX6XUu; refreshToken=aLqt956L5cF3zznybzHh8M1AQ6Y9tC7VuASJ2Kz7DoO3d-ghq6cLL1X8gRb9B_FbjTlgoIAR_Jx3dQdwObZ7Ye3KUm6iQiz_aCCI--V3ALsicbQWftq2RO7n4QNKs5wFcYh8hUsFF1BcNUDH1H1bdSqTL3FBtnMFwaWDnKT7WL0; _hjSessionUser_1632105=eyJpZCI6IjNiOTYyMWRmLThjNzEtNTAwMi1hNDA2LWI0OTlkNjEzNmFhNyIsImNyZWF0ZWQiOjE2Njk2MzkxNjgwNjYsImV4aXN0aW5nIjp0cnVlfQ==; _ga=GA1.3.1633265526.1669639167; _gat_UA-151320494-8=1; _ga_4ZXGB89LRC=GS1.1.1669639166.1.1.1669639244.0.0.0"
+					"cookie": "_gid=GA1.3.1470628385.1669639167; ln_or=d; _hjFirstSeen=1; _hjSession_1632105=eyJpZCI6IjExMTNjNTEzLWQ3MDktNDE3NS04MzRlLWQyMTBhN2EyMTkzOSIsImNyZWF0ZWQiOjE2Njk2MzkxNjgwOTksImluU2FtcGxlIjpmYWxzZX0=; token=" + encodeURIComponent(auth) + "; strategy=token; session=GFL8fGeCM67q7JfJZL95aBIe8RNX6XUu; refreshToken=aLqt956L5cF3zznybzHh8M1AQ6Y9tC7VuASJ2Kz7DoO3d-ghq6cLL1X8gRb9B_FbjTlgoIAR_Jx3dQdwObZ7Ye3KUm6iQiz_aCCI--V3ALsicbQWftq2RO7n4QNKs5wFcYh8hUsFF1BcNUDH1H1bdSqTL3FBtnMFwaWDnKT7WL0; _hjSessionUser_1632105=eyJpZCI6IjNiOTYyMWRmLThjNzEtNTAwMi1hNDA2LWI0OTlkNjEzNmFhNyIsImNyZWF0ZWQiOjE2Njk2MzkxNjgwNjYsImV4aXN0aW5nIjp0cnVlfQ==; _ga=GA1.3.1633265526.1669639167; _gat_UA-151320494-8=1; _ga_4ZXGB89LRC=GS1.1.1669639166.1.1.1669639244.0.0.0"
 				},
 				"method": "GET"
 			});
